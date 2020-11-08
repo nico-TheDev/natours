f0917e4debbb82e0961ee9cf7b57ff07d0796633
Norberto Ignacio <norbertoignaciojr@gmail.com>
2020-11-08 20:08:40 +0800

    remove unnecessary middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,7 +8,7 @@ const router = Router();
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+  .post(tourController.createTour);
 
 router
   .route('/:id')
